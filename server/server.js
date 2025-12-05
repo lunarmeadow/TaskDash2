6a04c18ffd364e27e3bb3cfc9eab30b074bee89e
Ashley Scott <ashley68k@proton.me>
2025-12-05 17:38:12 -0500

    Fix server and build targets for Railway

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
 import app from './express.js'
 import config from './config/config.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
+})
 
 app.listen(config.port, (err) => {
   if (err) {
